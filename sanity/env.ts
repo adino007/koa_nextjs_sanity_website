5d7dee2ec58e01de2a6c91b51dbe00d7e2699672
Adam Alakeli <alakeli@chapman.edu>
2024-07-16 21:49:19 -0700

    Beta 1 - Packages installed, eveything here works with empty home page through sanity into nextjs

---
+++ sanity/env.ts
@@ -0,0 +1,3 @@
+console.log('SANITY_PROJECT_ID:', process.env.NEXT_PUBLIC_SANITY_PROJECT_ID)
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+export const BASE_URL = process.env.NEXT_PUBLIC_SANITY_PREVIEW_URL
